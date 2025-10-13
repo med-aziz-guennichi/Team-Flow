585cecc8fa098ac5b18db93f23a365d05f32bdaf
Aziz Guennichi <medazizguennichi@gmail.com>
2025-10-13 22:21:14 +0100

    feat: add CreateWorkSpace component with form validation and integrate into WorkspaceLayout

--- app/(dashboard)/workspace/layout.tsx
+++ app/(dashboard)/workspace/layout.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { WorkspaceList } from './_components/workspace-list'
+import { CreateWorkSpace } from './_components/create-workspace'
 
 const WorkspaceLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex w-full h-screen'>
       <div className='flex h-full w-16 flex-col items-center bg-secondary py-3 px-2 border-r border-border'>
         <WorkspaceList />
+
+        <div className='mt-4'>
+          <CreateWorkSpace />
+        </div>
       </div>
     </div>
   )
